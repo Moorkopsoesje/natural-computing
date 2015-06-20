a8657af454958725289e928dd7e080136179ef2e
Sennaa <senna_vaniersel@hotmail.com>
2015-06-20 18:07:01 +0200

    Implemented most of algorithms
    TODO: get fitness function for states

--- js/game_manager.js
+++ js/game_manager.js
@@ -288,3 +288,69 @@ GameManager.prototype.fitnessnoweights = function () {
 	
 	return x + n;
 };
+
+
+// Strategies //
+
+// Random
+GameManager.prototype.random = function() {
+	return Math.floor((Math.random() * 4));		// returns next move, random number (0, 1, 2 or 3)
+};
+
+// Greedy
+GameManager.prototype.greedy = function() {
+	var direction = 0; 	// Initially, go up
+	// FF Up:
+	var ffup = this.fitness(0);
+	// FF Right:
+	var ffright = this.fitness(1);
+	if(Math.max(ffup, ffright) == ffright) {
+		direction = 1;
+	}
+	// FF Down:
+	var ffdown = this.fitness(2);
+	if(Math.max(ffup, ffright, ffdown) == ffdown) {
+		direction = 2;
+	}
+	// FF Left:
+	var ffleft = this.fitness(3);
+	if(Math.max(ffup, ffright, ffdown, ffleft) == ffleft) {
+		direction = 3;
+	}
+	return direction;
+};
+
+GameManager.prototype.fitness = function(direction) {
+	var previousState = this.storageManager.getGameState();
+	this.move(direction);
+	
+	// TODO: determine fitness function of current state
+	var fitness;
+	
+	this.storageManager.setGameState(previousState);
+	return fitness;
+}
+
+// Human
+GameManager.prototype.human = function() {	
+	// TODO: check if moves are valid / possible, if not valid, change move
+	if(this.lastmove == 3) { 		// Left
+		this.lastmove = 0;
+		return 0; 					// Up
+	}
+	else if (this.lastmove == 0) { 	// Up
+		this.lastmove = 3;
+		return 3;					// Left
+	}
+	else if (this.lastmove == 1) {	// Right
+		this.lastmove = 3;
+		return 3;					// Left
+	}
+	else if (this.lastmove == 2) {	// Down
+		this.lastmove = 0;
+		return 0;					// Up
+	}
+	else {
+		return -1;					// Should not come here
+	}
+};
