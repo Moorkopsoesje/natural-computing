527687aaafc0c7cb594afed78f0cae4265ad5bec
inezwijnands <inezwijnands@gmail.com>
2015-06-25 11:20:37 +0200

    Add mutation function

--- js/genome.js
+++ js/genome.js
@@ -4,6 +4,34 @@ function Genome(random, alphabeta, human) {
 	this.human 		= human;
 }
 
-Genome.prototype.update = function () {
-	
+var mutation_prob = 0.25;
+var mutation_amount = 0.05;
+
+Genome.prototype.mutation = function () {
+	var prob = new Array(10);
+	for (i = 0; i < 10; i++) {
+		prob[i] = Math.random();
+	}
+	if (prob[0] <= mutation_prob) {
+		if (prob[1] <= 0.5) {
+			this.random = this.random + mutation_amount;
+		}
+		else this.random = this.random - mutation_amount;
+	}
+	if (prob[2] <= mutation_prob) {
+		if (prob[3] <= 0.5) {
+			this.alphabeta = this.alphabeta + mutation_amount;
+		}
+		else this.alphabeta = this.alphabeta - mutation_amount;
+	}
+	if (prob[4] <= mutation_prob) {
+		if (prob[5] <= 0.5) {
+			this.human = this.human + mutation_amount;
+		}
+		else this.human = this.human - mutation_amount;
+	}
+	var normalization = this.random + this.alphabeta + this.human;
+	this.random = this.random/normalization;
+	this.alphabeta = this.alphabeta/normalization;
+	this.human = this.human/normalization;
 };
